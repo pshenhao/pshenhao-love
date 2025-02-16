b8400c19ca14e90611eb021151b2c84b9235c247
pengshenhao <76389343+pshenhao@users.noreply.github.com>
2025-02-16 18:35:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ let clickCount = 0;  // 记录点击 No 的次数
 
 // No 按钮的文字变化
 const noTexts = [
-    "？你认真的吗…", 
-    "要不再想想？", 
-    "不许选这个！ ", 
-    "我会很伤心…", 
+    "谁点的否？你认真的吗…", 
+    "还点否？选旁边的！！", 
+    "呜呜呜不许再点这个啦！ ", 
+    "我好伤心，快选旁边的！", 
     "不行:("
 ];
 
@@ -49,7 +49,7 @@ noButton.addEventListener("click", function() {
 yesButton.addEventListener("click", function() {
     document.body.innerHTML = `
         <div class="yes-screen">
-            <h1 class="yes-text">!!!喜欢你!! ( >᎑<)♡︎ᐝ</h1>
+            <h1 class="yes-text">!!!我的好闺蜜嘿嘿嘿!!! ( >᎑<)♡︎ᐝ</h1>
             <img src="images/hug.png" alt="拥抱" class="yes-image">
         </div>
     `;
